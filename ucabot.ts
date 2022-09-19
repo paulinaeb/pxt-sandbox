ca86e2651f5bf6a8d32a23669c15d0bd431b57b5
paulinaeb <pdespejo18@gmail.com>
2022-09-19 13:33:30 -0400

    validates id

--- ucabot.ts
+++ ucabot.ts
@@ -3,10 +3,10 @@
  */
 //% weight=5 color=#0fbc11  icon="\uf207" 
 namespace ucaBot {
-const STM8_ADDRESSS = 0x10
-let IR_Val = 0 
-let agent_id = '0'
-let ID_GROUP = 23
+const STM8_ADDRESSS = 0x10;
+let IR_Val = 0;
+let agent_id = '0';
+let ID_GROUP = 23;
     let _initEvents = true
 	/**
 	* Unit of Ultrasound Module
