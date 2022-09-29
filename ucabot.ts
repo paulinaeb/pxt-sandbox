e1be4fa955a08a10c16979444ef19357f0f4679a
paulinaeb <pdespejo18@gmail.com>
2022-09-29 14:44:48 -0400

    mark v2

--- ucabot.ts
+++ ucabot.ts
@@ -192,7 +192,7 @@ namespace ucaBot {
   /**
    * TODO: Initialize agent with color and Id.
    */
-  //% block="Initialize agent on Sandbox"
+  //% block="Initialize agent on Sandbox v2"
   //% weight=200 color=#ff9da5
   export function initAgent(): void {
     radio.setGroup(ID_GROUP);
