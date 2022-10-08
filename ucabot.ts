0576402a0f8b5ed70c9ba69d27147150f75cdadf
paulinaeb <pdespejo18@gmail.com>
2022-10-08 12:52:48 -0400

    more adjust

--- ucabot.ts
+++ ucabot.ts
@@ -530,16 +530,16 @@ namespace ucaBot {
           if (Math.abs(d_theta) > 300)
             d_theta = 360 + d_theta;
           if ((d_theta != 0) && (Math.abs(d_theta) > 1)){
-            vc = pid(Math.abs(d_theta), 2, 15, 5, 10);
+            vc = pid(Math.abs(d_theta), 2, 15, 6, 12);
             console.log('vc '+vc);
             // got to left, adjust to right
             if (d_theta < 0){
-              motors(v, v - vc);
+              motors(v + vc, v - vc);
               console.log('adjust to right');
             }
             else{
               // got to right, adjust to left 
-              motors(v - vc, v);
+              motors(v - vc, v + vc);
               console.log('adjust to left');
             }
           }
