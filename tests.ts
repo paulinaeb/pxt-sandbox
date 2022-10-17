c00d0bd2224fa5c25a4b8ecc5c227862df0065e5
paulinaeb <pdespejo18@gmail.com>
2022-10-17 12:31:40 -0400

    rm files unused, commented tests

--- tests.ts
+++ tests.ts
@@ -7,8 +7,8 @@ basic.showLeds(`
     . # . . .
     `)
 basic.forever(function () {
-    ucaBot.motors(30, 90)
+    // ucaBot.motors(30, 90)
     basic.pause(1500)
-    ucaBot.motors(90, 30)
+    // ucaBot.motors(90, 30)
     basic.pause(1500)
 })
