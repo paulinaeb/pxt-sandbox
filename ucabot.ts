dc817a73b23b488b39207342188d128b73cef5f5
paulinaeb <pdespejo18@gmail.com>
2022-10-17 11:43:20 -0400

    rm cutebot code

--- ucabot.ts
+++ ucabot.ts
@@ -5,8 +5,6 @@
 namespace ucaBot {
   const STM8_ADDRESSS = 0x10;
   const ID_GROUP = 23;
-  let IR_Val = 0;
-  let _initEvents = true;
 
   // class for responses received by radio
   class Resp { 
@@ -59,15 +57,6 @@ namespace ucaBot {
   let y_o: number = null;
   let id_ob = '';
   /**
-   * Unit of Ultrasound Module
-   */
-  export enum SonarUnit {
-    //% block="cm"
-    Centimeters,
-    //% block="inches"
-    Inches,
-  }
-  /**
    * Select the position desired
    */
   export enum Position {
@@ -95,26 +84,6 @@ namespace ucaBot {
     M2 = 1,
   }
   /**
-   * Select the servo on the S1 or S2
-   */
-  export enum ServoList {
-    //% block="S1"
-    S1 = 0,
-    //% block="S2"
-    S2 = 1,
-  }
-  /**
-   * Select the RGBLights on the left or right
-   */
-  export enum RGBLights {
-    //% blockId="Right_RGB" block="Right_RGB"
-    RGB_L = 1,
-    //% blockId="Left_RGB" block="Left_RGB"
-    RGB_R = 0,
-    //% blockId="ALL" block="ALL"
-    ALL = 3,
-  }
-  /**
    * Status List of Tracking Modules
    */
   export enum TrackingState {
@@ -130,77 +99,7 @@ namespace ucaBot {
     //% block="◌ ◌" enumval=3
     L_R_unline,
   }
-  export enum Direction {
-    //% block="Forward" enumval=0
-    forward,
-    //% block="Backward" enumval=1
-    backward,
-    //% block="Left" enumval=2
-    left,
-    //% block="Right" enumval=3
-    right,
-  }
-  /**
-   * Line Sensor events    MICROBIT_PIN_EVT_RISE
-   */
-  export enum MbEvents {
-    //% block="Found"
-    FindLine = DAL.MICROBIT_PIN_EVT_FALL,
-    //% block="Lost"
-    LoseLine = DAL.MICROBIT_PIN_EVT_RISE,
-  }
-  /**
-   * Pins used to generate events
-   */
-  export enum MbPins {
-    //% block="Left"
-    Left = DAL.MICROBIT_ID_IO_P13,
-    //% block="Right"
-    Right = DAL.MICROBIT_ID_IO_P14,
-  }
-  /**
-   * IR controller button
-   */
-  export enum IRButtons {
-    //% blcok="Menu"
-    Menu = 2,
-    //% blcok="Up"
-    Up = 5,
-    //% blcok="Left"
-    Left = 8,
-    //% blcok="Right"
-    Right = 10,
-    //% blcok="Down"
-    Down = 13,
-    //% blcok="OK"
-    OK = 9,
-    //% blcok="Plus"
-    Plus = 4,
-    //% blcok="Minus"
-    Minus = 12,
-    //% blcok="Back"
-    Back = 6,
-    //% block="0"
-    Zero = 14,
-    //% block="1"
-    One = 16,
-    //% block="2"
-    Two = 17,
-    //% block="3"
-    Three = 18,
-    //% block="4"
-    Four = 20,
-    //% block="5"
-    Five = 21,
-    //% block="6"
-    Six = 22,
-    //% block="7"
-    Seven = 24,
-    //% block="8"
-    Eight = 25,
-    //% block="9"
-    Nine = 26,
-  }
+
   /**
    * TODO: Initialize agent with an ID on Sandbox.
    */
@@ -638,12 +537,12 @@ namespace ucaBot {
   //% weight=160 color=#ff9da5
   //% block="Who are at least %d cm near me?"
   //% d.min = 12 d.max = 100
-  // export function nearMe(d: number): string { 
-  //   if (sendMsg('0', 'WN', [d.toString()], true, -1))
-  //     return near_me;
-  //   else
-  //     return '0'
-  // }
+  export function nearMe(d: number): string { 
+    if (sendMsg('0', 'WN', [d.toString()], true, -1))
+      return near_me;
+    else
+      return '0'
+  }
 /**
  * TODO: An agent can aks for other agent's help when needed
  */
@@ -817,103 +716,6 @@ namespace ucaBot {
     }
   }
   /**
-   * TODO: Full speed operation lasts for 10 seconds,speed is 100.
-   * @param dir Driving direction, eg: Direction.forward
-   * @param speed Running speed, eg: 50
-   * @param time Travel time, eg: 5
-   */
-  //% blockId=ucaBot_move_time block="Go %dir at speed%speed\\% for %time seconds"
-  //% weight=95
-  export function moveTime(dir: Direction, speed: number, time: number): void {
-    if (dir == 0) {
-      motors(speed, speed);
-      basic.pause(time * 1000);
-      motors(0, 0);
-    }
-    if (dir == 1) {
-      motors(-speed, -speed);
-      basic.pause(time * 1000);
-      motors(0, 0);
-    }
-    if (dir == 2) {
-      motors(-speed, speed);
-      basic.pause(time * 1000);
-      motors(0, 0);
-    }
-    if (dir == 3) {
-      motors(speed, -speed);
-      basic.pause(time * 1000);
-      motors(0, 0);
-    }
-  }
-  /**
-   * TODO: full speed move forward,speed is 100.
-   */
-  //% blockId=ucaBot_forward block="Go straight at full speed"
-  //% weight=90
-  export function forward(): void {
-    // Add code here
-    let buf = pins.createBuffer(4);
-    buf[0] = 0x01;
-    buf[1] = 0x02;
-    buf[2] = 80;
-    buf[3] = 0;
-    pins.i2cWriteBuffer(STM8_ADDRESSS, buf);
-    buf[0] = 0x02;
-    pins.i2cWriteBuffer(STM8_ADDRESSS, buf);
-  }
-
-  /**
-   * TODO: full speed move back,speed is -100.
-   */
-  //% blockId=ucaBot_back block="Reverse at full speed"
-  //% weight=85
-  export function backforward(): void {
-    // Add code here
-    let buf = pins.createBuffer(4);
-    buf[0] = 0x01;
-    buf[1] = 0x01;
-    buf[2] = 80;
-    buf[3] = 0;
-    pins.i2cWriteBuffer(STM8_ADDRESSS, buf);
-    buf[0] = 0x02;
-    pins.i2cWriteBuffer(STM8_ADDRESSS, buf);
-  }
-  /**
-   * TODO: full speed turnleft.
-   */
-  //% blockId=ucaBot_left block="Turn left at full speed"
-  //% weight=80
-  export function turnleft(): void {
-    // Add code here
-    let buf = pins.createBuffer(4);
-    buf[0] = 0x02;
-    buf[1] = 0x02;
-    buf[2] = 80;
-    buf[3] = 0;
-    pins.i2cWriteBuffer(STM8_ADDRESSS, buf);
-    buf[0] = 0x01;
-    buf[2] = 0;
-    pins.i2cWriteBuffer(STM8_ADDRESSS, buf);
-  }
-  /**
-   * TODO: full speed turnright.
-   */
-  //% blockId=ucaBot_right block="Turn right at full speed"
-  //% weight=75
-  export function turnright(): void {
-    // Add code here
-    let buf = pins.createBuffer(4);
-    buf[0] = 0x01;
-    buf[1] = 0x02;
-    buf[2] = 80;
-    buf[3] = 0;
-    pins.i2cWriteBuffer(STM8_ADDRESSS, buf);
-    buf[0] = 0x02;
-    buf[2] = 0;
-    pins.i2cWriteBuffer(STM8_ADDRESSS, buf);
-  }
-  /**
    * TODO: stopcar
    */
   //% blockId=ucaBot_stopcar block="Stop car immediatly"
@@ -921,78 +723,6 @@ namespace ucaBot {
   export function stopcar(): void {
     motors(0, 0);
   }
-  /**
-   * TODO: Set LED headlights.
-   */
-  //% block="Set LED headlights %light color $color"
-  //% color.shadow="colorNumberPicker"
-  //% weight=65
-  export function colorLight(light: RGBLights, color: number) {
-    let r: number,
-      g: number,
-      b: number = 0;
-    r = color >> 16;
-    g = (color >> 8) & 0xff;
-    b = color & 0xff;
-    singleheadlights(light, r, g, b);
-  }
-  /**
-   * TODO: Select a headlights and set the RGB color.
-   * @param R R color value of RGB color, eg: 0
-   * @param G G color value of RGB color, eg: 128
-   * @param B B color value of RGB color, eg: 255
-   */
-  //% inlineInputMode=inline
-  //% blockId=RGB block="Set LED headlights %light color R:%r G:%g B:%b"
-  //% r.min=0 r.max=255
-  //% g.min=0 g.max=255
-  //% b.min=0 b.max=255
-  //% weight=60
-  export function singleheadlights(
-    light: RGBLights,
-    r: number,
-    g: number,
-    b: number
-  ): void {
-    let buf = pins.createBuffer(4);
-    if (light == 3) {
-      buf[0] = 0x04;
-      buf[1] = r;
-      buf[2] = g;
-      buf[3] = b;
-      pins.i2cWriteBuffer(STM8_ADDRESSS, buf);
-      buf[0] = 0x08;
-      pins.i2cWriteBuffer(STM8_ADDRESSS, buf);
-    } else {
-      if (light == 0) {
-        buf[0] = 0x04;
-      }
-      if (light == 1) {
-        buf[0] = 0x08;
-      }
-      buf[1] = r;
-      buf[2] = g;
-      buf[3] = b;
-      pins.i2cWriteBuffer(STM8_ADDRESSS, buf);
-    }
-  }
-  /**
-   * Close all headlights.
-   */
-  //% inlineInputMode=inline
-  //% block="Turn off all LED headlights"
-  //% weight=55
-  export function closeheadlights(): void {
-    let buf = pins.createBuffer(4);
-    buf[0] = 0x04;
-    buf[1] = 0;
-    buf[2] = 0;
-    buf[3] = 0;
-    pins.i2cWriteBuffer(STM8_ADDRESSS, buf);
-    buf[0] = 0x08;
-    pins.i2cWriteBuffer(STM8_ADDRESSS, buf);
-  }
-
   /**
    * Judging the Current Status of Tracking Module.
    * @param state Four states of tracking module, eg: TrackingState.L_R_line
@@ -1014,126 +744,6 @@ namespace ucaBot {
       return true;
     } else {
       return false;
-    }
-  }
-  /**
-   * TODO: track one side
-   * @param side Line sensor edge , eg: MbPins.Left
-   * @param state Line sensor status, eg: MbEvents.FindLine
-   */
-  //% block="%side line sensor %state"
-  //% state.fieldEditor="gridpicker" state.fieldOptions.columns=2
-  //% side.fieldEditor="gridpicker" side.fieldOptions.columns=2
-  //% weight=45
-  export function trackSide(side: MbPins, state: MbEvents): boolean {
-    pins.setPull(DigitalPin.P13, PinPullMode.PullNone);
-    pins.setPull(DigitalPin.P14, PinPullMode.PullNone);
-    let left_tracking = pins.digitalReadPin(DigitalPin.P13);
-    let right_tracking = pins.digitalReadPin(DigitalPin.P14);
-    if (side == 0 && state == 1 && left_tracking == 1) {
-      return true;
-    } else if (side == 0 && state == 0 && left_tracking == 0) {
-      return true;
-    } else if (side == 1 && state == 1 && right_tracking == 1) {
-      return true;
-    } else if (side == 1 && state == 0 && right_tracking == 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  /**
-   * TODO: Runs when line sensor finds or loses.
-   */
-  //% block="On %sensor| line %event"
-  //% sensor.fieldEditor="gridpicker" sensor.fieldOptions.columns=2
-  //% event.fieldEditor="gridpicker" event.fieldOptions.columns=2
-  //% weight=40
-  export function trackEvent(sensor: MbPins, event: MbEvents, handler: Action) {
-    initEvents();
-    control.onEvent(<number>sensor, <number>event, handler);
-  }
-  /**
-   * Cars can extend the ultrasonic function to prevent collisions and other functions..
-   * @param Sonarunit two states of ultrasonic module, eg: Centimeters
-   */
-  //% blockId=ultrasonic block="HC-SR04 Sonar unit %unit"
-  //% weight=35
-  export function ultrasonic(unit: SonarUnit, maxCmDistance = 500): number {
-    // send pulse
-    pins.setPull(DigitalPin.P8, PinPullMode.PullNone);
-    pins.digitalWritePin(DigitalPin.P8, 0);
-    control.waitMicros(2);
-    pins.digitalWritePin(DigitalPin.P8, 1);
-    control.waitMicros(10);
-    pins.digitalWritePin(DigitalPin.P8, 0);
-    // read pulse
-    const d = pins.pulseIn(DigitalPin.P12, PulseValue.High, maxCmDistance * 50);
-    switch (unit) {
-      case SonarUnit.Centimeters:
-        return Math.floor((d * 34) / 2 / 1000);
-      case SonarUnit.Inches:
-        return Math.floor(((d * 34) / 2 / 1000) * 0.3937);
-      default:
-        return d;
-    }
-  }
-  /**
-   * TODO: Set the angle of servo.
-   * @param Servo ServoList , eg: ucaBot.ServoList.S1
-   * @param angle angle of servo, eg: 90
-   */
-  //% blockId=ucaBot_servo block="Set servo %servo angle to %angle °"
-  //% angle.shadow="protractorPicker"
-  //% weight=30
-  export function setServo(Servo: ServoList, angle: number = 180): void {
-    let buf = pins.createBuffer(4);
-    if (Servo == ServoList.S1) {
-      buf[0] = 0x05;
-      buf[1] = angle;
-      buf[2] = 0;
-      buf[3] = 0; //补位
-      pins.i2cWriteBuffer(STM8_ADDRESSS, buf);
-    } else {
-      buf[0] = 0x06;
-      buf[1] = angle;
-      buf[2] = 0;
-      buf[3] = 0; //补位
-      pins.i2cWriteBuffer(STM8_ADDRESSS, buf);
-    }
-  }
-  //% shim=IRV2::irCode
-  function irCode(): number {
-    return 0;
-  }
-  //% weight=25
-  //% block="On IR receiving"
-  export function IR_callback(handler: () => void) {
-    pins.setPull(DigitalPin.P16, PinPullMode.PullUp);
-    control.onEvent(98, 3500, handler);
-    control.inBackground(() => {
-      while (true) {
-        IR_Val = irCode();
-        if (IR_Val != 0xff00) {
-          control.raiseEvent(98, 3500, EventCreationMode.CreateAndFire);
-        }
-        basic.pause(20);
-      }
-    });
-  }
-  /**
-   * TODO: Get IR value
-   */
-  //% block="IR Button %Button is pressed"
-  //% weight=15
-  export function IR_Button(Button: IRButtons): boolean {
-    return (IR_Val & 0x00ff) == Button;
-  }
-  function initEvents(): void {
-    if (_initEvents) {
-      pins.setEvents(DigitalPin.P13, PinEventType.Edge);
-      pins.setEvents(DigitalPin.P14, PinEventType.Edge);
-      _initEvents = false;
     }
   }
 }
